3d6f19132e0e575ba54add22ece51746e13ab2b2
durgaprasad-palanati-AI <dp.cse5@gmail.com>
2021-05-24 19:01:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My first React applicication
+        </p>
+        <a
+          className="App-link"
+          href="https://mail.google.com/mail/u/0/#inbox"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        
+      </header>
+      <h1>DURGA PRASAD</h1>
+    </div>
+  );
+}
+
+export default App;
